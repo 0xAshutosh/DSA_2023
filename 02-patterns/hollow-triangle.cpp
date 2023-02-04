b15580d35cd45ee96bd88c6e2fbcdce0055e8cad
Ashutosh Singh <su.ashutosh@gmail.com>
2023-02-04 23:04:12 +0530

    Added some more pattern

---
+++ 02-patterns/hollow-triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int n;
+    // cin >> n;
+    // bool a = false;
+    int a = 5;
+//    cout << ~(a) << endl;
+   cout << (++a) * (++a) << endl;
+}
