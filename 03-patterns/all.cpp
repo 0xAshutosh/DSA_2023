3124c61efe509b9f0d3c3738177ae38185dd4667
Ashutosh Singh <su.ashutosh@gmail.com>
2023-02-14 21:08:21 +0530

    Revised pattern

--- 03-patterns/all.cpp
+++ 03-patterns/all.cpp
@@ -59,7 +59,49 @@ int main() {
     //     // print stars;
     //     for (int col = 0; col < n - row; col++) cout << "* ";
     //     cout << endl; 
+    // } 
+
+    // for (int row = 0; row < n; row++) {
+    //     // print spaces; 
+    //     for (int space = 0; space < n - row - 1; space++) cout << ' ';
+    //     // print numbers;
+    //     for (int col = 0; col < row + 1; col++) cout << row + col + 1; 
+    //     // print num in reverse;
+    //     int num = 2 * row; 
+    //     for (int col = 0; col < row; col++) cout << num--;  
+    //     cout << endl;
+    // } 
+
+    // numeric hollow full pyramid; 
+    // for (int row = 0; row < n; row++) {
+    //     // print spaces;
+    //     for (int space = 0; space < n - row - 1; space++) cout << ' ';
+    //     // print numbers;
+    //     int num = 2;
+    //     for (int col = 0; col < 2 * row + 1; col++) {
+    //         if (col == 0) cout << 1; 
+    //         else if (col == 2 * row) cout << row + 1;
+    //         else if (row == n - 1 && col % 2 == 0) cout << num++;
+    //         else cout << ' '; 
+    //     }
+    //     cout << endl; 
     // }
+
+    for (int row = 0; row < n; row++) {
+        // print spaces;
+        for (int space = 0; space < n - row - 1; space++) cout << ' ';
+        // print stars;
+        for (int col = 0; col < row + 1; col++) cout << "* ";
+        cout << endl;
+    }
+
+    for (int row = 0; row < n; row++) {
+        // print spaces;
+        for (int space = 0; space < row; space++) cout << ' ';
+        // print stars;
+        for (int col = 0; col < n - row; col++) cout << "* "; 
+        cout << endl;  
+    }
 
    
     return 0;
