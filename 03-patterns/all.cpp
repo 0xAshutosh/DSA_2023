7d36858b1fd55cb54ecf15b7162482ef90f1167a
Ashutosh Singh <su.ashutosh@gmail.com>
2023-02-14 18:11:30 +0530

    Revised pattern

---
+++ 03-patterns/all.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int m, n;
+    // cin >> m >> n;
+
+    // for (int row = 0; row < m; row++) {
+    //     for (int col = 0; col < n; col++) cout << '*';
+    //     cout << endl;
+    // }
+
+    // for (int row = 0; row < m; row++) {
+    //     if (row == 0 || row == m - 1) {
+    //         for (int col = 0; col < n; col++) cout << "*";
+    //     } else {
+    //         cout << "*";
+    //         for (int col = 0; col < n - 2; col++) cout << ' ';
+    //         cout << "*";
+    //     }
+
+    //     cout << endl;
+    // }
+
+    int n;
+    cin >> n;
+
+    // for (int row = 0; row < n; row++) {
+    //     for (int col = 0; col < row + 1; col++) cout << '*';
+    //     cout << endl;
+    // }
+
+    // for (int row = 0; row < n; row++) {
+    //     for (int col = 0; col < n - row; col++) cout << '*';
+    //     cout << endl;
+    // }
+
+    // for (int row = 0; row < n; row++) {
+    //     for (int col = 0; col < row + 1; col++) cout << col + 1;
+    //     cout << endl; 
+    // }
+
+    // for (int row = 0; row < n; row++) {
+    //     for (int col = 0; col < n - row; col++) cout << col + 1;
+    //     cout << endl; 
+    // }
+
+    // for (int row = 0; row < n; row++) {
+    //     // print space;
+    //     for (int space = 0; space < n - row - 1; space++) cout << ' ';
+    //     // print stars;
+    //     for (int col = 0; col < row + 1; col++) cout << "* ";
+    //     cout << endl;
+    // }
+
+    // for (int row = 0; row < n; row++) {
+    //     // print spaces;
+    //     for (int space = 0; space < row; space++) cout << ' ';
+    //     // print stars;
+    //     for (int col = 0; col < n - row; col++) cout << "* ";
+    //     cout << endl; 
+    // }
+
+   
+    return 0;
+}
