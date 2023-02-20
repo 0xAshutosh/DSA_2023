722d6d56f6d3669bb34482ec4e07a2d747084699
Ashutosh Singh <su.ashutosh@gmail.com>
2023-02-20 21:31:41 +0530

    Coded decimal to binary using bitwise operators

--- number-system/decimaltobinary.cpp
+++ number-system/decimaltobinary.cpp
@@ -14,11 +14,36 @@ int decimalToBinary(int n) {
     return ans;
 }
 
+int decimalToBinary2(int n) {
+    int ans = 0, base = 1;
+
+    while (n != 0) {
+        int bit = (n & 1);
+        ans = bit * base + ans;
+        base *= 10;
+        n = n >> 1; 
+    }
+
+    return ans; 
+}
+
+int binaryToDecimal(long n) {
+    long ans = 0, base = 1;
+    while (n != 0) {
+        int rem = n % 10;
+        ans = rem * base + ans;
+        n /= 10;
+        base *= 2;
+    }
+
+    return ans; 
+}
+
 int main() {
     int n;
     cin >> n; 
 
-    cout << "Answer is " << decimalToBinary(n) << endl;
+    cout << "Answer is " << decimalToBinary2(n) << endl;
 
     return 0;
 }
