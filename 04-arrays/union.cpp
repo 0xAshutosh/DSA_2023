20e43d86f1e59d49b64b5db27085670eb705629c
Ashutosh Singh <su.ashutosh@gmail.com>
2023-02-27 23:13:22 +0530

    20230227

--- 04-arrays/union.cpp
+++ 04-arrays/union.cpp
@@ -3,17 +3,11 @@
 using namespace std;
 
 vector<int> findUnion(vector<int>& arr1, vector<int>& arr2) {
-    vector<int> ans;
-    for (int i = 0; i < arr1.size(); i++) {
-        for (int j = i; j < arr2.size(); j++) {
-            if (arr1[i] == arr2[j]) ans.push_back(arr1[i]); 
-            else {
-                ans.push_back(arr2[j]);
-            }
-        }
-        ans.push_back(arr1[i]); 
-    }
-    return ans;
+    vector<int> ans; 
+    for (int i = 0; i < arr1.size(); i++) ans.push_back(arr1[i]); 
+    for (int i = 0; i < arr2.size(); i++) ans.push_back(arr2[i]); 
+
+    return ans; 
 }
 
 int main() {
